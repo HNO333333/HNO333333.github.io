b119a6f5bc263f2fdaeaef6c49b3c89ef966712f
HNO3_computer <HNO333333@sina.com>
2023-09-01 21:57:40 +0800

    fix date bug?

--- quartz/components/Date.tsx
+++ quartz/components/Date.tsx
@@ -1,6 +1,12 @@
+import { GlobalConfiguration } from "../cfg"
+import { QuartzPluginData } from "../plugins/vfile"
+
 interface Props {
   date: Date
 }
+
+export type ValidDateType = keyof Required<QuartzPluginData>["dates"]
+
 
 export function formatDate(d: Date): string {
   return d.toLocaleDateString("en-US", {
@@ -12,4 +18,13 @@ export function formatDate(d: Date): string {
 
 export function Date({ date }: Props) {
   return <>{formatDate(date)}</>
+}
+
+export function getDate(cfg: GlobalConfiguration, data: QuartzPluginData): Date | undefined {
+  if (!cfg.defaultDateType) {
+    throw new Error(
+      `Field 'defaultDateType' was not set in the configuration object of quartz.config.ts. See https://quartz.jzhao.xyz/configuration#general-configuration for more details.`,
+    )
+  }
+  return data.dates?.[cfg.defaultDateType]
 }
